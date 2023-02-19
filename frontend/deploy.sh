c60c66e3e08b517e2cc4642eae86f94d853f4d43
std-013-60 std-013-60 <std-013-60@praktikum-services.ru>
2023-02-19 15:24:24 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,10 +6,9 @@ sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-fro
 sudo rm -f /home/jarservice/sausage-store-frontend.tar.gz||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-frontend.tar.gz ${NEXUS_FRONTEND_REPO_URL}/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo cp ./sausage-store-frontend.tar.gz /home/jarservice/sausage-store-frontend.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай
 #Распакуем архив в нужную папку и скопируем файлы в директорию фронтэнда
-sudo tar -xzvf /home/jarservice/sausage-store-frontend.tar.gz
-sudo cp -a /home/jarservice/frontend/. /var/www-data/dist/frontend/
+sudo tar -xzvf ./sausage-store-frontend.tar.gz
+sudo cp -a ./frontend/. /var/www-data/dist/frontend/
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
