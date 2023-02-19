84f6da6289a38cc51ec43a66518e84c6dac18867
std-013-60 std-013-60 <std-013-60@praktikum-services.ru>
2023-02-19 15:17:39 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,7 +6,7 @@ sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-fro
 sudo rm -f /home/jarservice/sausage-store-frontend.tar.gz||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-frontend.tar.gz ${NEXUS_FRONTEND_REPO_URL}/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo cp /home/$DEV_USER/sausage-store-frontend.tar.gz /home/jarservice/sausage-store-frontend.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай
+sudo cp ./sausage-store-frontend.tar.gz /home/jarservice/sausage-store-frontend.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай
 #Распакуем архив в нужную папку и скопируем файлы в директорию фронтэнда
 sudo tar -xzvf /home/jarservice/sausage-store-frontend.tar.gz
 sudo cp -a /home/jarservice/frontend/. /var/www-data/dist/frontend/
