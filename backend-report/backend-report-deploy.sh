b5b38f275e3640f7cf99ba0681e7e945c9a261a4
std-013-60 std-013-60 <std-013-60@praktikum-services.ru>
2023-05-03 11:44:18 +0000

    Update backend-report-deploy.sh

--- backend-report/backend-report-deploy.sh
+++ backend-report/backend-report-deploy.sh
@@ -10,13 +10,13 @@ GIT_FOLDER=${GIT_FOLDER}
 VERSION=${VERSION}
 EOF
 
-cp report.env $GIT_FOLDER/backend-report/
-
+cp backend-report.env $GIT_FOLDER/backend-report/
+cd ..
 docker network create -d bridge sausage_network || true
 docker login -u $GITLAB_USER -p $GITLAB_PASS $GITLAB_REGISTRY
 docker pull ${GITLAB_REGISTRY}/sausage-store/sausage-backend:latest
 docker stop backend || true
-eocker rm backend || true
+docker rm backend || true
 docker-compose up -d backend-report \
     --network=sausage_network \
     --restart always \
