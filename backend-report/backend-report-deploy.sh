eff47bcc7a5b3b9c00878730f70add44e3741b9f
std-013-60 std-013-60 <std-013-60@praktikum-services.ru>
2023-05-03 12:56:46 +0000

    Update backend-report-deploy.sh

--- backend-report/backend-report-deploy.sh
+++ backend-report/backend-report-deploy.sh
@@ -2,6 +2,7 @@
 set -xe
 
 cat > backend-report.env <<EOF
+DB=${SPRING_DATA_MONGODB_URI}?tls=true&tlsCaFile=YandexInternalRootCA.crt
 CI_PROJECT_DIR=${CI_PROJECT_DIR}
 GITLAB_REGISTRY=${GITLAB_REGISTRY}
 GITLAB_USER=${GITLAB_USER}
