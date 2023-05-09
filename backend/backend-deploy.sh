6860e0ac2fe431003068906a0de9606fad444ca6
Alex Savin <root@fhmojh2h9altpf5biv2u.auto.internal>
2023-05-09 10:27:25 -0300

    making all correct

--- backend/backend-deploy.sh
+++ backend/backend-deploy.sh
@@ -19,7 +19,5 @@ docker login -u $GITLAB_USER -p $GITLAB_PASS $GITLAB_REGISTRY
 docker pull ${GITLAB_REGISTRY}/sausage-store/sausage-backend:latest
 docker-compose stop backend || true
 docker-compose up -d backend \
-    --network=sausage_network \
-    --restart always \
     --pull always \
     ${GITLAB_REGISTRY}/sausage-store/sausage-backend:latest
