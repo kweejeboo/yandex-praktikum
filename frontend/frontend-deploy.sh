b53166b31f6283496551c730208671462836622f
std-013-60 std-013-60 <std-013-60@praktikum-services.ru>
2023-05-15 08:08:30 +0000

    Update frontend-deploy.sh

--- frontend/frontend-deploy.sh
+++ frontend/frontend-deploy.sh
@@ -13,7 +13,8 @@ cd $GIT_FOLDER/frontend/
 docker network create -d bridge sausage_network || true
 docker login -u $GITLAB_USER -p $GITLAB_PASS $GITLAB_REGISTRY
 docker pull ${GITLAB_REGISTRY}/sausage-store/sausage-frontend:latest
-docker-compose stop sausage-store-frontend  || true
+docker-compose stop frontend  || true
+docker-compose rm frontend || true
 
 set -e
 docker-compose up -d frontend 
